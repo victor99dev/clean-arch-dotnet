8dcc7c264174d3e363aab3a99ff2599a910097c2
Victor Hugo <victormotabatista@gmail.com>
2023-06-01 17:47:34 -0300

    feat: create DependencyInjection

---
+++ src/Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,26 @@
+using Domain.Interfaces;
+using Infra.Data.Context;
+using Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
