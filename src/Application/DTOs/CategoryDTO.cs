0e98d98bbcc1a120eba8563b2765c01d40315f6d
Victor Hugo <victormotabatista@gmail.com>
2023-06-03 13:23:16 -0300

    feat: creating the dtos

---
+++ src/Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public Guid id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string name {get; set; }
+
+        public bool is_active {get; set; }
+
+        public DateTime created_at { get; set; }
+
+        public DateTime updated_at { get; set; }
+    }
+}
---
+++ src/Application/DTOs/ProductDTO.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entities;
+
+namespace Application.DTOs
+{
+    public class ProductDTO
+    {
+        public Guid id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "The Description is Required")]
+        [MinLength(5)]
+        [MaxLength(100)]
+        public string description { get; set; }
+
+        [Required(ErrorMessage = "The Price is Required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0.C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal price { get; set; }
+
+        [Required(ErrorMessage = "The Stock is Required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string image { get; set; }
+
+        public bool is_active { get; set; }
+
+        public Category category { get; set; }
+
+        [DisplayName("Categories")]
+        public Guid category_id { get; set; }
+
+        public DateTime created_at { get; set; }
+
+        public DateTime updated_at { get; set; }
+    }
+}
