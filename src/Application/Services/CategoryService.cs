2ce65ed41da8bb3656d0cbbd136865f2b5c5aab8
Victor Hugo <victormotabatista@gmail.com>
2023-06-03 15:43:35 -0300

    feat: category and product service

---
+++ src/Application/Services/CategoryService.cs
@@ -0,0 +1,49 @@
+using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategoriesAsync()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategoriesAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+        }
+
+        public async Task<CategoryDTO> GetByIdAsync(string? id)
+        {
+            var categorieEntity = await _categoryRepository.GetByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(categorieEntity);
+        }
+
+        public async Task CreateAsync(CategoryDTO categoryDTO)
+        {
+            var categorieEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.CreateAsync(categorieEntity);
+        }
+
+        public async Task UpdateAsync(CategoryDTO categoryDTO)
+        {
+            var categorieEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.RemoveAsync(categorieEntity);
+        }
+
+        public async Task RemoveAsync(string? id)
+        {
+            var categorieEntity = _categoryRepository.GetByIdAsync(id).Result;
+            await _categoryRepository.RemoveAsync(categorieEntity);
+        }
+    }
+}
---
+++ src/Application/Services/ProductService.cs
@@ -0,0 +1,56 @@
+using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentNullException(nameof(productRepository)); 
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProductsAsync()
+        {
+            var productsEntity = await _productRepository.GetProductsAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task<ProductDTO> GetByIdAsync(string? id)
+        {
+            var productEntity = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategoryAsync(string? id)
+        {
+            var productEntity = await _productRepository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task CreateAsync(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateAsync(productEntity);
+        }
+
+        public async Task UpdateAsync(ProductDTO productDTO)
+        {
+             var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateAsync(productEntity);
+        }
+
+        public async Task RemoveAsync(string? id)
+        {
+             var productEntity = _productRepository.GetByIdAsync(id).Result;
+            await _productRepository.RemoveAsync(productEntity);
+        }
+    }
+}
