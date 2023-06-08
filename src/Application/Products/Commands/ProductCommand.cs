a5a657b5936b0939e324390d334899b8f44b1556
Victor Hugo <victormotabatista@gmail.com>
2023-06-08 02:49:39 -0300

    remove null

--- src/Application/Products/Commands/ProductCommand.cs
+++ src/Application/Products/Commands/ProductCommand.cs
@@ -9,7 +9,7 @@ namespace Application.Products.Commands
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public string? Image { get; set; }
+        public string Image { get; set; }
         public Guid CategoryId { get; set; }
     }
 }
