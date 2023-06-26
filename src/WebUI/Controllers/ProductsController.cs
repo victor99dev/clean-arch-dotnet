723d65a2d71d4228c5835da67a90fa8c25da122a
Victor Hugo <victormotabatista@gmail.com>
2023-06-26 15:35:14 -0300

    creating the product edit view

--- src/WebUI/Controllers/ProductsController.cs
+++ src/WebUI/Controllers/ProductsController.cs
@@ -37,12 +37,40 @@ namespace WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(ProductDTO product)
         {
-           if (ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 await _productService.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
             return View(product);
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null) return NotFound();
+
+            var product = await _productService.GetByIdAsync(id);
+
+            if (product == null) return NotFound();
+
+            var categories = await _categoryService.GetCategoriesAsync();
+
+            ViewBag.CategoryId = new SelectList(categories, "id", "name", product.category_id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.UpdateAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
     }
 }
