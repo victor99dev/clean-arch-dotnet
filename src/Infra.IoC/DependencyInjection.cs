b926045439a3af9241320d64e8b2a0904d5ca7bf
Victor Hugo <victormotabatista@gmail.com>
2023-07-22 22:43:27 -0300

    feat(identity): including services and identity settings in the infra.ioc layer

--- src/Infra.IoC/DependencyInjection.cs
+++ src/Infra.IoC/DependencyInjection.cs
@@ -1,9 +1,12 @@
 using Application.Interfaces;
 using Application.Mappings;
 using Application.Services;
+using Domain.Account;
 using Domain.Interfaces;
 using Infra.Data.Context;
+using Infra.Data.Identity;
 using Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,9 +15,7 @@ namespace Infra.IoC
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(
-            this IServiceCollection services,
-            IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
@@ -26,11 +27,21 @@ namespace Infra.IoC
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
 
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             var myhandlers = AppDomain.CurrentDomain.Load("Application");
 
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(myhandlers));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureExternalCookie(options =>
+                options.AccessDeniedPath = "/Account/Login");
 
             return services;
         }
