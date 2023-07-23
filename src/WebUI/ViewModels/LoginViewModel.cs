6acfa0f66ccf8f22a11fe6c535efd678f2346774
Victor Hugo <victormotabatista@gmail.com>
2023-07-23 03:53:47 -0300

    create the new view models for login

---
+++ src/WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max " + "{1} characters long.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
